b006e14270db845948f90462386fadf238d6edbf
Marissa Pratt <131078835+heeyitsrissa@users.noreply.github.com>
2023-12-24 14:39:37 -0500

    added prompt to starter code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,19 +4,37 @@
 // validate input 
 // password should generate criteria
 // passwords should be generated on screen
-let passLength = 8 - 128;
+let passLength;
+
+let password = ``;
 
 let upperLetter = [`A`, `B`, `C`, `D`, `E`, `F`, `G`, `H`, `I`, `J`, `K`, `L`, `M`, `N`, `O`, `P`, `Q`, `R`, `S`, `T`, `U`, `V`, `W`, `X`, `Y`, `Z`];
 
-let lowLetter = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`];
+const lowLetter = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`, `i`, `j`, `k`, `l`, `m`, `n`, `o`, `p`, `q`, `r`, `s`, `t`, `u`, `v`, `w`, `x`, `y`, `z`];
 
-let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-let specialSymbol = [`!`, `@`, `#`, `$`, `%`, `^`, `&`, `*`, `,`, `.`, `?`];
+const specialSymbol = [`!`, `@`, `#`, `$`, `%`, `^`, `&`, `*`, `,`, `.`, `?`];
 
 const generateBtn = document.querySelector("#generate");
 
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+
+    passLength = prompt (`How Long would you like your password to be?`);
+    if(passLength <= 7){
+        alert(`you need at least 8 characters`);
+    } else if (passLength >= 129){
+        alert(`you can not have more than 128 characters`);
+    } 
+    upperLetter = confirm(`Would you like to use upper case letters?`);
+    if(upperLetter){
+        
+    }
+
+}
+
+
+
 
 
 function writePassword() {
@@ -29,6 +47,7 @@ function writePassword() {
     
 }
 
-function generatePassword()
-   
 
+
+
+generateBtn.addEventListener("click", writePassword);
